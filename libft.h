d07fb84fb8e1eed11768377132a3483e4b18be07
PIVOx11 <brahimhossam160@gmail.com>
2025-09-15 12:08:57 +0100

    first part done

---
+++ libft.h
@@ -0,0 +1,7 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <string.h>
+
+size_t  ft_strlen(const char *s);
+q
+#endif
---
+++ mai.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main ()
+{
+    printf("%zu\n",ft_strlen("Hello studet"));
+}
