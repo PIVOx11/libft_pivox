72375f89c4ff18fce853cdc590fbd624b5fbc951
PIVOx11 <brahimhossam160@gmail.com>
2025-09-29 21:13:55 +0100

    add bzero

---
+++ ft_bzero.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void    ft_bzero(void *s, size_t n)
+{
+    size_t i;
+    char *ptr = (char *)s;
+
+    i = 0;
+    while(i < n)
+    {
+        ptr[i] = '0';
+        i++;
+    }
+}
